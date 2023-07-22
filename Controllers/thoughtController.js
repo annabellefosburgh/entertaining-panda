57ca375c062a196283bb87f937f8224d38dcc73e
annabellefosburgh <afosburgh20@gmail.com>
2023-07-22 13:58:13 -0500

    Writing thought controller page

---
+++ Controllers/thoughtController.js
@@ -0,0 +1,127 @@
+//Dependencies from the model folder
+const { Thought, User } = require('../Models');
+
+//Wrapping all functions to export
+const thoughtPath = {
+    //Grabs all thoughts
+    getThoughts(req, res) {
+        Thought.find()
+        .sort({createdAt: -1})
+        .then((thoughtData) => {
+            res.json(thoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    //Grabs one thought by id
+    getOneThought(req, res) {
+        Thought.findOne({_id: req.params.thoughtId})
+        .then((thoughtData) => {
+            res.json(thoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    //Make a new thought
+    newThought(req, res) {
+      Thought.create(req.body)
+      .then((thoughtData) => {
+        return User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $push: {thoughts: thoughtData._id} },
+            { new: true}
+        );
+      })
+      .then((thoughtData) => {
+        res.json({ message: "Thought created!"});
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })  
+    },
+
+    //Update one thought by id
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((thoughtData) => {
+            res.json(thoughtData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    //Delete one thought by id
+    deleteThought(req, res) {
+        Thought.findOneAndRemove(
+            { _id: req.params.thoughtId }
+            
+        )
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+
+        //Removing thought from user it belongs to
+        return User.findOneAndUpdate(
+            { thoughts: req.params.thoughtId },
+            { $pull: { thoughts: req.params.thoughtId } },
+            { new: true }
+        )
+        .then((userData) => {
+            res.json({ message: "Thought deleted!" })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    //Creating reaction to add to specific thought
+    newReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body} },
+            { runValidators: true, new: true }
+        )
+        .then(thoughtData => {
+            res.json(thoughtData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    //Remove reaction from specific thought
+    removeReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: {reactionId: req.params.reactionId }}},
+            { runValidators: true, new: true }
+        )
+        .then((thoughtData) => {
+            res.json(thoughtData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+
+};
+
+module.exports = thoughtPath
