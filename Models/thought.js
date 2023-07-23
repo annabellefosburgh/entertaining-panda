1e4149e0b7571eb18dc37c2392b57092d8bf01a8
annabellefosburgh <afosburgh20@gmail.com>
2023-07-23 10:30:31 -0500

    Writing schema for thought model

--- Models/thought.js
+++ Models/thought.js
@@ -1,3 +1,35 @@
+//Requiring mongoose, our reaction model, and our helper as dependencies
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+const dateFormat = require("../utils/dateFormat");
 
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: "Message must not be left blank.",
+            minlength: 1,
+            maxlength: 300,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reaction: [reactionSchema],
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+    }
+);
 
+//Something to return the length of the reaction count
 
+module.exports = thoughtSchema;
